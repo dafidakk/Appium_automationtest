9d703539bc3e4f8f41d107e9aeb917f57088655b
Ahmet Fatih DEVECI <ahmetfatihdeveci@gmail.com>
2022-06-21 17:37:43 +0300

    Test_APK install & inspect

--- appiumTest/installApk.py
+++ appiumTest/installApk.py
@@ -10,13 +10,25 @@ desired_caps = {}
 
 desired_caps['deviceName'] = 'Android'
 desired_caps['platformName'] = 'Android'
-desired_caps['app'] = str(Path().absolute().parent) + '\\app\\keep.apk'
+#desired_caps['app'] = str(Path().absolute().parent) + '\\app\\keep.apk'
+desired_caps['app'] = str(Path().absolute().parent) + '\\app\\hepsiburadanew.apk'
 
 
 driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
-driver.implicitly_wait(5)
+driver.implicitly_wait(10)
+
+el1 = driver.find_element(by=AppiumBy.ID, value="com.pozitron.hepsiburada:id/textViewSearchBox")
+el1.click()
+el2 = driver.find_element(by=AppiumBy.ID, value="com.pozitron.hepsiburada:id/etACBSearchBox")
+el2.click()
+el2.send_keys("Araba anahtarı")
+
+el3 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[1]")
+el3.click()
+
 time.sleep(2)
-#driver.quit()
+driver.quit()
+
 
 
